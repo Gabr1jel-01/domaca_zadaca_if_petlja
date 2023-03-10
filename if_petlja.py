7999032fdfc4797ec5c192395115af4bb9b690e7
Gabr1jel-01 <76105935+Gabr1jel-01@users.noreply.github.com>
2023-03-10 14:36:11 +0100

    Gotova zadaca

---
+++ if_petlja.py
@@ -0,0 +1,24 @@
+
+
+'''
+U generickom tekstu Lorem Ipsum pronadite koliko se puta ponavlja neka rijec:
+
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+'''
+
+
+lorem_ipsum_text = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'''
+
+list_of_repeating_words = []
+
+for word in lorem_ipsum_text.lower().split():
+    
+    if word == 'ut':
+        list_of_repeating_words.append(word)
+        
+print(f'Rijec ut se ponavlja {len(list_of_repeating_words)} puta')
